386923a953674f7b2626c16f5101d5022c863281
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-02-23 23:51:39 -0400

    add basicc signup form

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.3'
 
+gem 'bcrypt'
 #Bootstrap
 gem 'bootstrap', '~> 4.4.1'
 #jquery for boostrap
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
+  resources :users, only: [:new, :create]
+  
+  get '/signup' => 'users#new' 
+  post '/signup' => 'users#create'
+
+  
 end
