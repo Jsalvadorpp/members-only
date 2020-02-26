f6ed317a3350a2822626054fab007bd0fb2b87da
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-02-26 18:18:28 -0400

    create post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_140541) do
+ActiveRecord::Schema.define(version: 2020_02_26_221140) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "bodyText"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -23,4 +32,5 @@ ActiveRecord::Schema.define(version: 2020_02_26_140541) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
