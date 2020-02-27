980fa7c30c6bee18eb33301a0719815bb87a453e
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-02-26 22:11:23 -0400

    add post routes protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     include SessionsHelper
+
+    def logged
+        unless is_logged?
+            flash['danger'] = 'please log in'
+            redirect_to login_path
+        end
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,14 @@
 class PostsController < ApplicationController
+    before_action :logged , only: [:new,:create]
 
     def index
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
     end
 
 
