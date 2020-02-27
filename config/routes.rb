1d6e92c3e3184c0c8b33f6b3fda0c597b21cafbf
Jsalvadorpp <josesalvadorpp@hotmail.com>
2020-02-26 22:10:57 -0400

    add new post form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index' , as: 'home'
   resources :users, only: [:new, :create]
+  resources :posts , only: [:new, :create]
   
   get '/signup' => 'users#new'  , as: 'signup'
   post '/signup' => 'users#create'
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#login' , as: 'login'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#logout' , as: 'logout'
+
+
 
   
 end
